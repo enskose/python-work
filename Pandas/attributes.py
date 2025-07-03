8dfe26d8db6e63353427107eacad8583181152b5
enskose <enskose1@gmail.com>
2025-07-03 21:31:57 +0300

    updated

---
+++ Pandas/attributes.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+# python nesneler şunları içerir:
+# data, metadata, related functionalities
+
+# bir nesne hakkında meta veri sağlayan değişkene öznitelik (attribute) denir 
+# bir nesne ile ilişkilendirilebilen bir fonksiyona metod (method) denir
+
+series_a = pd.Series([10, 20, 30, 40, 50])
+
+
+
+
